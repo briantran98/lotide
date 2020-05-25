f5b2056cf7b776a40b5b9952d10594aac50aa9fb
Brian Tran <98brian.tran@gmail.com>
2020-05-25 23:56:33 +0000

    Added assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return "✅✅✅ Assertion Passed: " + actual + " === " + expected;
+  } else {
+    return "🛑🛑🛑 Assertion Failed: " + actual + " === " + expected;
+  }
+
+};
+
+
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual("bootcamp", "Bootcamp"));
+console.log(assertEqual("Bootcamp", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual(1, 2));
+console.log(assertEqual(1, "1"));
