d64816ed0045b96f49654904570c0d17dcede3c2
Brian Tran <98brian.tran@gmail.com>
2020-05-26 23:42:06 +0000

    Added assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,11 @@
+const assertArraysEqual = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return `🛑🛑🛑 Assertion Failed: ${array1} === ${array2}`;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return `🛑🛑🛑 Assertion Failed: ${array1} === ${array2}`;
+    }
+  }
+  return `✅✅✅ Assertion Passed: ${array1} === ${array2}`;
+}
