3b05bf7a0a936e38bbb9ab187934759a843dfb08
Brian Tran <98brian.tran@gmail.com>
2020-05-30 01:03:11 +0000

    Changes made

--- eqArrays.js
+++ eqArrays.js
@@ -6,16 +6,23 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+
 const eqArrays = function(array1, array2) {
+  let same = false;
   if (array1.length !== array2.length) {
     return false;
   }
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
+    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+      same = eqArrays(array1[i], array2[i])
+    } else {
+      same = false;
+    }
+    if (array1[i] === array2[i]) {
+      same = true;
     }
   }
-  return true;
+  return same
 };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),  true);
@@ -23,3 +30,9 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),  true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])) // => true
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])) // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])) // => false
+
