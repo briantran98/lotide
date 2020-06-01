498845a40fe2b64253d05c97067c7c99255e507d
Brian Tran <98brian.tran@gmail.com>
2020-06-01 19:37:31 +0000

    Refactored and separated test code

---
+++ test/assertArraysEqual.js
@@ -0,0 +1,6 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+assertArraysEqual([1,2,3], [1,2,3]);
+assertArraysEqual([3,2,1], [3,2,1]);
+assertArraysEqual([], []);
+assertArraysEqual(["1","2","3"], ["1","2","3"]);
