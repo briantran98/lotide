8dea87eff59061a881c2fdf913d7a692ea12c0dc
Brian Tran <98brian.tran@gmail.com>
2020-06-01 19:23:42 +0000

    Removed test cases from tail.js and moved it to test dir

--- tail.js
+++ tail.js
@@ -1,28 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
-  let newArray = [];
-  
-  for (let i = 1; i < array.length; i++) {
-    newArray.push(array[i]);
-  }
-  
+  let newArray = array.filter( (element) => element !== array[0]);
   return newArray;
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-console.log(assertEqual(words.length, 3)); // original array should still have 3 elements!
-console.log(assertEqual(tail(words)[0], "Lighthouse"));
-console.log(assertEqual(tail(words).length, 2));
-console.log(assertEqual(tail(words)[0], "Labs"));
-
-
-
+module.exports = tail;
