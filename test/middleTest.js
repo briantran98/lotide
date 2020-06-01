d89ff928822671fd913a9189817e64bc63f417db
Brian Tran <98brian.tran@gmail.com>
2020-06-01 20:25:33 +0000

    Added mocha and chai testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,4 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
 const numbers1 = [1, 2, 3, 4, 5, 6, 7, 8];
@@ -6,7 +6,25 @@ const numbers2 = [1, 2, 3, 4, 5, 6, 7];
 const numbers3 = [1, 2, 3, 4, 5, 6];
 const numbers4 = [1, 2, 3, 4, 5];
 
-assertArraysEqual(middle(numbers2), [4]);
-assertArraysEqual(middle(numbers4), [3]);
-assertArraysEqual(middle(numbers3), [3,4]);
-assertArraysEqual(middle(numbers1), [4,5]);
+describe('#middle', () => {
+  it('return 4 for [1, 2, 3, 4, 5, 6, 7]', () => {
+    const actual = middle(numbers2);
+    const expected = [4];
+    assert.deepEqual(actual, expected);
+  });
+  it('return 4 for [1, 2, 3, 4, 5]', () => {
+    const actual = middle(numbers4);
+    const expected = [3];
+    assert.deepEqual(actual, expected);
+  });
+  it('return 4 for [1, 2, 3, 4, 5, 6]', () => {
+    const actual = middle(numbers3);
+    const expected = [3, 4];
+    assert.deepEqual(actual, expected);
+  });
+  it('return 4 for [1, 2, 3, 4, 5, 6, 7, 8]', () => {
+    const actual = middle(numbers1);
+    const expected = [4, 5];
+    assert.deepEqual(actual, expected);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,36 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+
+describe('#tail', () => {
+  it('returns 3 for the length of the original array after calling tail', () => {
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  });
+  it('returns "Lighthouse" for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    const actual = tail(words)[0];
+    const expected = 'Lighthouse';
+    assert.strictEqual(actual, expected);
+  });
+  it('returns 2 length for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    const actual = tail(words).length;
+    const expected = 2;
+    assert.strictEqual(actual, expected);
+  });
+  it('returns "Lighthouse" for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    const actual = tail(words)[1];
+    const expected = 'Labs';
+    assert.strictEqual(actual, expected);
+  });
+});
+
+
+
+/* // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
 assertEqual(tail(words)[0], "Lighthouse");
 assertEqual(tail(words).length, 2);
-assertEqual(tail(words)[1], "Labs");
+assertEqual(tail(words)[1], "Labs"); */
